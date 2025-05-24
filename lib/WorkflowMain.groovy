9923c35355d3752b7e5280c5578a054c6a57515e
FerriolCalvet <ferriolcalvet@gmail.com>
2025-05-24 21:26:19 +0200

    refactor: update ext.prefix closures for consistency and readability; comment out unused genome attribute method

--- lib/WorkflowMain.groovy
+++ lib/WorkflowMain.groovy
@@ -106,16 +106,16 @@ class WorkflowMain {
         }
     }
 
-    //
-    // Get attribute from genome config file e.g. fasta
-    //
-    public static String getGenomeAttribute(params, attribute) {
-        def val = ''
-        if (params.genomes && params.genome && params.genomes.containsKey(params.genome)) {
-            if (params.genomes[ params.genome ].containsKey(attribute)) {
-                val = params.genomes[ params.genome ][ attribute ]
-            }
-        }
-        return val
-    }
+    // //
+    // // Get attribute from genome config file e.g. fasta
+    // //
+    // public static String getGenomeAttribute(params, attribute) {
+    //     def val = ''
+    //     if (params.genomes && params.genome && params.genomes.containsKey(params.genome)) {
+    //         if (params.genomes[ params.genome ].containsKey(attribute)) {
+    //             val = params.genomes[ params.genome ][ attribute ]
+    //         }
+    //     }
+    //     return val
+    // }
 }
